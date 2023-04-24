f2f292575e8a0f4d9853200d16647868172a6e67
jonrato <jonatas_ferreira@usp.br>
2023-04-24 07:34:53 -0300

    API Gateway Integration

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,11 +1,13 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { Context } from "vm";
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
 export async function handler(event: APIGatewayProxyEvent,
     context: Context): Promise<APIGatewayProxyResult> {
         
         const lambdaRequestId = context.awsRequestId
         const apiRequestId = event.requestContext.requestId
+
+        console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`)
+
         const method = event.httpMethod
         if (event.resource === "/products") {
         if (method === 'GET') {
